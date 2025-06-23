0373703499133f5b3b5eb9475aaa38e75b8b9fee
kalfawaz <khaled.alfawaz1212@gmail.com>
2025-06-23 15:55:53 +0300

    stage config

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,7 @@ module.exports = (webpackConfigEnv, argv) => {
         template: "src/index.ejs",
         templateParameters: {
           isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
+          isStage: webpackConfigEnv && webpackConfigEnv.isStage,
           orgName,
         },
       }),
